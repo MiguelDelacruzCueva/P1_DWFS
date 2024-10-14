3e022c38ecdf60409dfbc29268b641686e98d115
Jpiter25 <161729263+MiguelDelacruzCueva@users.noreply.github.com>
2024-10-13 19:54:13 -0500

    Add files via upload

---
+++ practica_web/script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const navList = document.querySelector('.nav-list');
+    navList.classList.toggle('active');
+}
+function cambiarCSSoriginal()
+{
+    document.getElementById("cssestilo").href = "diseño.css"
+}
+
+function cambiarCSSalterno()
+{
+    document.getElementById("cssestilo").href = "estilo1.css"
+}
